cd39dd6f77200333e81feedfbd01c19eb498dc52
legbing <spoorthikalkunte@gmail.com>
2023-02-14 21:38:24 +0530

    Updated hello.cpp for webhook

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Update! I have added a webhook trigger to the repo and the build has been automated by Jenkins on push!\n";
   return 0;
 }
