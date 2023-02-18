39f8fa4b4621d796d83b7bbad25fc2db688e18fe
legbing <spoorthikalkunte@gmail.com>
2023-02-18 09:31:18 +0530

    Added the cpp file for testing pipeline

---
+++ pipeline_PES1UG20CS433.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "Update! I have added a webhook trigger to the repo and the build has been automated by Jenkins on push!\n";
+  cout << "Update update! I have created a pipeline script using Groovy to compile and run a C++ file!\n";
+  return 0;
+}
